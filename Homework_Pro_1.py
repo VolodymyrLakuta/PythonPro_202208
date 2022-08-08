24cb15d36fcf047374bab658bf5ed45afc6ca9f4
VolodymyrLakuta <110728458+VolodymyrLakuta@users.noreply.github.com>
2022-08-08 21:03:58 +0300

    Add files via upload
    Після розбору на лекції

--- Homework_Pro_1.py
+++ Homework_Pro_1.py
@@ -20,18 +20,24 @@ class Buyers:
 class Orders:
     def __init__(self, buyer: Buyers):
         self.order = []
+        self.quantities = []
         self.buyer = buyer
-        self.total = 0
-        
-    def add_item (self, item: Goods):
+          
+    def add_item (self, item: Goods, quantity: int):
         self.order.append(item)
-        self.total += item.price   
+        self.quantities.append(quantity)
+
+    def total(self):
+        res = 0
+        for i, item in enumerate(self.order):
+            res += item.price * self.quantities[i]
+        return res 
 
     def __str__(self):
         order_str = f"Your order: \n{self.buyer}\n"
-        for i in range(1, len(self.order)+1):
-            order_str += f"\nItem {i}: {self.order[i-1]}\n"
-        order_str += f"\nTotal: {len(self.order)} Item (-s) on ${self.total}"
+        for i, item in enumerate(self.order):
+            order_str += f"\nItem {i+1}: {item}, Q-ty: {self.quantities[i]}\n"
+        order_str += f"\nTotal: {len(self.order)} Item (-s) on ${self.total()}"
         return order_str 
    
 goods_1 = Goods("Dumbbells Set 10 kg", 50.0, "25*10*10 cm each", "Set of 2 dumbbells of 10 kg each, cast iron")
@@ -40,10 +46,20 @@ goods_3 = Goods("Dumbbells Set 5 kg", 30.0, "15*6*6 cm each", "Set of 2 dumbbell
 
 buyer_1 = Buyers("Ivanov", "Petro", "+380991234567")
 
-order = Orders(buyer_1)
+order_1 = Orders(buyer_1)
 
-order.add_item(goods_1) 
-order.add_item(goods_2)
-order.add_item(goods_3)  
+order_1.add_item(goods_1, 1) 
+order_1.add_item(goods_2, 2)
+order_1.add_item(goods_3, 3)  
 
-print (order)
+print (order_1)
+
+buyer_2 = Buyers("Petrov", "Ivan", "+380997654321")
+
+order_2 = Orders(buyer_2)
+
+order_2.add_item(goods_1, 4)
+
+print()
+print (order_2)
+
