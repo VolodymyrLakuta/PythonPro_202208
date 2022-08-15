c1b5fda40718ce65f9c7be271789fdc4776fcf24
VolodymyrLakuta <110728458+VolodymyrLakuta@users.noreply.github.com>
2022-08-15 16:53:29 +0300

    Add files via upload

---
+++ Homework_Pro_3_1.py
@@ -0,0 +1,39 @@
+class Goods:
+    def __init__(self, name : str, price : float):
+        self.name = name
+        self.price = price
+
+    def price_control (name, price):   
+        try:
+            if not is_number_float (price) or not float(price):
+                raise My_exceptions ("Invalid price characters")
+        except My_exceptions as err:
+            print(err.get_exception_message())
+        else:
+            price = round(float(price), 2)
+            good = Goods(name, price)
+            return good
+        
+    def  __str__(self):
+        return f"\n{self.name} Price: ${self.price}\n"
+
+class My_exceptions (Exception):
+    def __init__(self, message):
+        super().__init__()
+        self.message = message
+
+    def get_exception_message(self):
+        return self.message 
+
+def is_number_float (val):
+    try:
+        float(val)
+        return True 
+    except ValueError: 
+        return False
+
+name = input("Enter the goods name: ")
+price = input ("Enter the goods price($ or $.cents): ")
+good_1 = Goods.price_control (name, price)
+
+print(good_1)
