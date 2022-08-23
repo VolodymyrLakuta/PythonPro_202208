9299c62ab614891809e9150d922638c599d0721b
VolodymyrLakuta <110728458+VolodymyrLakuta@users.noreply.github.com>
2022-08-23 12:58:15 +0300

    Add files via upload

--- Group_Class.py
+++ Group_Class.py
@@ -1,3 +1,7 @@
+'''
+This module includes classes Group and GroupIter
+'''
+
 from Students_Class import Students
 
 from My_exceptions_Class import My_exceptions
@@ -5,32 +9,52 @@ from My_exceptions_Class import My_exceptions
 class Group:
     def __init__(self, title):
         self.title = title
-        self.group_list = []
+        self.__group_list = []
 
     def add_new_student (self, student : Students, MAX_GR = 10):
-        if len(self.group_list) == MAX_GR:
+        if len(self.__group_list) == MAX_GR:
             raise My_exceptions("The group is already dialed")
-        for item in self.group_list:
+        for item in self.__group_list:
             if item.surname.lower() == student.surname.lower() and \
                 item.name.lower() == student.name.lower() and \
                 item.year_birth == student.year_birth:
                 raise My_exceptions("This student is already in the group")
         
-        self.group_list.append(student)
+        self.__group_list.append(student)
         
     def search_student (self, student_name):         # Пошук студентів за призвіщем
-        for item in self.group_list:
+        for item in self.__group_list:
             if item.surname.lower() == student_name.lower():
                 return item
         raise My_exceptions("Not found")
 
     def del_student (self, student_name):              # Видалення студента (пошук за призвіщем)
-        for item in self.group_list:
+        for item in self.__group_list:
             if item.surname.lower() == student_name.lower():
-                self.group_list.remove(item)
+                self.__group_list.remove(item)
                 return
         raise My_exceptions("Not found")
      
     def __str__ (self):
-        return f'{self.title}\n' + '\n'.join(map(str, self.group_list))
+        return f'{self.title}\n' + '\n'.join(map(str, self.__group_list))
+
+    def __iter__(self):
+        return GroupIter(self.__group_list)
+
+
+class GroupIter:
+    def __init__(self, group_list):
+        self.group_list = group_list
+        self.index = 0
+
+    def __next__(self):
+        if self.index < len(self.group_list):
+            self.index += 1
+            return self.group_list[self.index - 1]
+        else:
+            raise StopIteration
+
+    def __iter__(self):
+        return self
+
 
