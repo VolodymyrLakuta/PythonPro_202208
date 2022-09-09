50b867930d1535e7f9a5fe8f3bc7e7bf7e3e4a08
Volodymyr Lakuta <110728458+VolodymyrLakuta@users.noreply.github.com>
2022-09-09 19:18:44 +0300

    Delete Homework_Pro_Students_6 directory

--- Homework_Pro_Students_6/Human_Class.py
+++
@@ -1,9 +0,0 @@
-class Human:
-    def __init__(self, surname : str, name : str, year_birth : str):
-        self.surname = surname
-        self.name = name
-        self.year_birth = year_birth
-      
-    def  __str__(self):
-        return f"{self.surname} {self.name}, {self.year_birth}\n"
-
