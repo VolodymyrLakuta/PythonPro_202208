6beeab5581a6a35cf731e388b4bb8527c966e0e1
Volodymyr Lakuta <110728458+VolodymyrLakuta@users.noreply.github.com>
2022-09-09 19:18:22 +0300

    Delete Homework_Pro_Goods_6 directory

--- Homework_Pro_Goods_6/Class_Buyers.py
+++
@@ -1,7 +0,0 @@
-class Buyers:
-    def __init__(self, surname, name, phonenumber):
-       self.surname = surname
-       self.name = name
-       
-    def  __str__(self):
-        return f"\nBuyer: {self.surname} {self.name}"
--- Homework_Pro_Goods_6/Class_My_exceptions.py
+++
@@ -1,8 +0,0 @@
-class My_exceptions (Exception):
-    def __init__(self, message):
-        super().__init__()
-        self.message = message
-
-    def get_exception_message(self):
-        return f"{self.message}"
-
